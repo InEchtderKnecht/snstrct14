cb67ee36aba7499fdfeddd7d362f76e6f592a0b4
RupertAdlmaier <rupert.adlmaier@medx.net>
2014-03-19 16:30:33 +0100

    init gradient version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,46 +2,40 @@
 
   'use strict';
 
-  //function setMainMargin($main) {
-  //  var wHeight = $(window).height(),
-  //    wWidth = $(window).width(),
-  //    mainTopPadding = parseInt($main.css('padding-top'), 10),
-  //    festivalTag = $main.find('.festival_tag').innerHeight();
-  //  console.log(mainTopPadding + festivalTag);
-  //  var margin = wHeight - (mainTopPadding * 2 + festivalTag);
-  //  if (wWidth < 600) {
-  //    //$main.css('margin-top', 'inherit');
-  //  } else {
-  //    //$main.css('margin-top', margin);
-  //  }
-  //}
-
   $(function () {
 
   // FireShell
 
     FastClick.attach(document.body);
     
-    var s = skrollr.init();
-    skrollr.menu.init(s, {
-        animate: true,
-        easing: 'swing'
-      });
+    //var s = skrollr.init();
+    //skrollr.menu.init(s, {
+    //    animate: true,
+    //    easing: 'swing'
+    //  });
 
     $(window).load(function(){
-      //setMainMargin($('.main'));
-      s.refresh();
+      //s.refresh();
     });
 
     $(window).on('resize', function(){
       //setMainMargin($('.main'));
     });
 
-    //$(document).on('click','.tag, .nav', function() {
-    //  $('body').toggleClass('showMenu');
+    //$('.svg').each(function(i, element) {
+    //  var watcher = scrollMonitor.create( element, -150 );
+    //  watcher.lock();
+    //  watcher.stateChange(function() {
+    //    if (this.isAboveViewport) {
+    //      $(element).addClass('state1');
+    //    } else {
+    //      $(element).removeClass('state1');
+    //    }
+    //  });
     //});
 
 
   });
 
 })(jQuery, window, document);
+
